8916aebbef9b701f93b7521fa3ccd44b6a29532c
Felipe Rooke <felipe@rooke.com.br>
2017-11-08 18:16:54 -0200

    Arrumando dependencias do Maven

---
+++ src/main/java/br/ufjf/pontoufjf/PontoUfjfApplication.java
@@ -0,0 +1,12 @@
+package br.ufjf.pontoufjf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoUfjfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoUfjfApplication.class, args);
+	}
+}
