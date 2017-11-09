7b0c223091abc4d5de7bafc64a98d4a80e3570dc
Felipe Rooke <felipe@rooke.com.br>
2017-11-09 16:07:52 -0200

    Criado Repositories e respectivos Testes

---
+++ src/main/java/br/ufjf/pontoufjf/repositories/EmpresaRepository.java
@@ -0,0 +1,14 @@
+package br.ufjf.pontoufjf.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.ufjf.pontoufjf.entities.Empresa;
+
+public interface EmpresaRepository extends JpaRepository<Empresa, Long>{
+	
+	@Transactional(readOnly = true)
+	Empresa findByCnpj(String cnpj);
+	
+	
+}
---
+++ src/test/java/br/ufjf/pontoufjf/repositories/EmpresaRepositoryTest.java
@@ -0,0 +1,55 @@
+package br.ufjf.pontoufjf.repositories;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import br.ufjf.pontoufjf.entities.Empresa;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+
+public class EmpresaRepositoryTest{
+	
+	@Autowired
+	private EmpresaRepository empresaRepository;
+	
+	private static final String CNPJ = "51463645000100";
+
+	/*
+	 * Executa operações antes da execução do teste
+	 */
+	@Before
+	public void setUp() throws Exception {
+		Empresa empresa = new Empresa();
+		empresa.setRazaoSocial("Empresa de exemplo");
+		empresa.setCnpj(CNPJ);
+		this.empresaRepository.save(empresa);
+	}
+	
+	/*
+	 * Executa operações depois da execução do Teste
+	 */
+	@After
+    public final void tearDown() { 
+		this.empresaRepository.deleteAll();
+	}
+
+	@Test
+	public void testBuscarPorCnpj() {
+		Empresa empresa = this.empresaRepository.findByCnpj(CNPJ);
+		
+		assertEquals(CNPJ, empresa.getCnpj());
+	}
+	
+	
+	
+}
